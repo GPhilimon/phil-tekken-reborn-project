e50196cd84591e3f33533ae21dce3a601951c0ef
brylle <philihab1@gmail.com>
2023-03-17 21:33:44 -0500

    lab 6

---
+++ src/main/java/com/cpan252/tekkenreborn/config/SecurityConfig.java
@@ -0,0 +1,57 @@
+package com.cpan252.tekkenreborn.config;
+
+import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import com.cpan252.tekkenreborn.model.User;
+import com.cpan252.tekkenreborn.repository.UserRepository;
+
+@Configuration
+public class SecurityConfig {
+
+   @Bean
+   public PasswordEncoder passwordEncoder() {
+      return new BCryptPasswordEncoder();
+   }
+
+   @Bean
+   public UserDetailsService userDetailsService(UserRepository userRepository) {
+      return username -> {
+         User user = userRepository.findByUsername(username);
+         if (user != null) {
+            return user;
+         }
+         throw new UsernameNotFoundException(username);
+      };
+   }
+
+   @Bean
+   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+      return http
+            .authorizeHttpRequests()
+            .requestMatchers(toH2Console()).permitAll()
+            .requestMatchers("/design", "/fighter_list")
+            .hasRole("USER")
+            .anyRequest().permitAll()
+            .and()
+            .formLogin(login -> login
+                  .loginPage("/login"))
+            .logout(logout -> logout
+                  .logoutSuccessUrl("/"))
+            .csrf(csrf -> csrf
+                  .ignoringRequestMatchers(toH2Console()))
+            .headers(headers -> headers
+                  .frameOptions()
+                  .sameOrigin())
+            .build();
+   }
+
+}
